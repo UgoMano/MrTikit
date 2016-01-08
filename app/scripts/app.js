b4d013a2cff5c1efd4df0a6dd5f4f0a9b806dac2
Steve Calabro <steve.a.calabro@gmail.com>
2016-01-07 19:39:27 -0500

    Based angular stuff with less, working routing, and bootstrap html5 template

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mrtikitApp
+ * @description
+ * # mrtikitApp
+ *
+ * Main module of the application.
+ */
+angular.module('mrtikitApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router',
+  ]).config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('app', {
+        url: "/",
+        templateUrl: "views/main.html",
+        controller: 'MainCtrl'
+    })
+
+    .state('about', {
+        url: "/about",
+        templateUrl: "views/about.html",
+        controller: 'AboutCtrl'
+    })
+
+    $urlRouterProvider.otherwise('/');
+}).run(function ($rootScope, $state, $http, $location, $window, $timeout) {
+    console.log($state);
+});
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mrtikitApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mrtikitApp
+ */
+angular.module('mrtikitApp').controller('MainCtrl', function () {
+     console.log('main')
+});
