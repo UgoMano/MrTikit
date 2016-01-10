85e81e50ce22772009c24bfdd75aee68da0506dd
Steve Calabro <steve.a.calabro@gmail.com>
2016-01-10 18:55:26 -0500

    Added mysql for django database

--- MrTikit/settings.py
+++ MrTikit/settings.py
@@ -91,10 +91,21 @@ WSGI_APPLICATION = 'MrTikit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'mrtikit',
+        'USER': 'mrtikit',
+        'PASSWORD': 'pGMDyr54Qvh8Ymy6',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
