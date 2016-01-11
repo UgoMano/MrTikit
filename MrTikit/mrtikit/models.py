fa6db824a5908c54e3049cf2157dd668bbb99ee3
Steve Calabro <steve.a.calabro@gmail.com>
2016-01-11 16:03:59 -0500

    Nightly

--- MrTikit/mrtikit/models.py
+++ MrTikit/mrtikit/models.py
@@ -1,15 +1,2 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractBaseUser
-
-#class User(AbstractBaseUser):
-#    """
-#    Custom user class.
-#    """
-#    email = models.EmailField('email address', unique=True, db_index=True)
-#
-#    #For django
-#    USERNAME_FIELD = 'email'
-#
-#    #human-readable representation
-#    def __unicode__(self):
-#        return self.email
--- MrTikit/mrtikit/serializers.py
+++ MrTikit/mrtikit/serializers.py
@@ -5,11 +5,12 @@ class SignUpSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
     first_name = serializers.CharField(required=True)
     last_name = serializers.CharField(required=True)
+    test_field = serializers.CharField(required=True)
 
     class Meta:
         model = User
 
-        fields = ('username', 'password', 'email', 'first_name', 'last_name')
+        fields = ('username', 'password', 'email', 'first_name', 'last_name', 'test_field')
         write_only_fields = ('password',)
 
     def create(self, validated_data):
