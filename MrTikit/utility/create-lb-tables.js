9958b470fc3585661db651b1a741a26c54b4f079
Steve Calabro <steve.a.calabro@gmail.com>
2016-02-18 17:00:54 -0500

    cleard for node

--- MrTikit/utility/create-lb-tables.js
+++
@@ -1,8 +0,0 @@
-var server = require('../server/server');
-var ds = server.dataSources.mysqlDB;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
-ds.automigrate(lbTables, function(er) {
-	if(er) throw er;
-	console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
-	ds.disconnect();
-});
