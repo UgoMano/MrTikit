622de27e3a2516f04c62f43f97ee4aa6f8691b47
Josh Bauer <jbauer@Joshs-MBP.fios-router.home>
2016-02-25 17:56:48 -0500

    added LoginCtrl

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mrtikitApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the mrtikitApp
+ */
+angular.module('mrtikitApp').controller('LoginCtrl', function () {
+     console.log('login')
+});
