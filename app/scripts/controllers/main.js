4037deeac3830cf07d35187b8997c0abc02342f1
Josh Bauer <jbauer@n2-163-254.dhcp.drexel.edu>
2016-02-25 20:25:27 -0500

    added sidebar icons collapse sidebar updated font awesome

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,6 +7,13 @@
  * # MainCtrl
  * Controller of the mrtikitApp
  */
-angular.module('mrtikitApp').controller('MainCtrl', function () {
+angular.module('mrtikitApp').controller('MainCtrl', function ($scope,$rootScope, $interval) {
      console.log('main')
+     if($rootScope.collapsed==null)
+         $rootScope.collapsed=false;
+     $rootScope.toggleCollapse = function() {
+         $rootScope.collapsed= !$rootScope.collapsed;
+         console.log("collapsed: ",$rootScope.collapsed);
+     };
+    //$interval($scope.toggleCollapse,1000);
 });
