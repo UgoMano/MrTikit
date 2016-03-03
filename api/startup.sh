4c1a83e08579b0e30608bb6bcf1d7158556cc563
Stephen Calabro <steve.a.calabro@gmail.com>
2016-03-03 18:53:34 -0500

    Added fix for npm api docker image

--- api/startup.sh
+++ api/startup.sh
@@ -2,11 +2,10 @@
 cd /MrTikit
 export GIT_SSH=/root/.ssh/git_wrap.sh
 export EMAIL=joshbauer3@gmail.com
-git fetch origin api-master --depth 1
-git reset --hard origin/api-master
+git fetch origin api-dev --depth 1
+git reset --hard origin/api-dev
 #npm install -g bower
 #bower install --allow-root --force-latest
-npm install -g npm
 npm install
 #STEVE ADD API COMMANDS HERE (download libs and startup)
 #sails lift --prod
