41ed16fad9d12f252992fe07bc36e6f7fc878265
Josh Bauer <jbauer@Joshs-MacBook-Pro.local>
2016-03-03 12:54:06 -0500

    updated dockerfile and startup script for api container

--- api/startup.sh
+++ api/startup.sh
@@ -4,4 +4,6 @@ export GIT_SSH=/root/.ssh/git_wrap.sh
 export EMAIL=joshbauer3@gmail.com
 git fetch origin api-master --depth 1
 git reset --hard origin/api-master
+npm install -g bower
+bower install --allow-root --force-latest
 #STEVE ADD API COMMANDS HERE (download libs and startup)
