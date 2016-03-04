7db3413119bd438a122d325552879d1c05669e64
Josh Bauer <jbauer@n2-47-70.dhcp.drexel.edu>
2016-03-03 20:59:14 -0500

    updated myEvents and eventOverview

--- app/scripts/providers/backendProvider.js
+++ app/scripts/providers/backendProvider.js
@@ -3,43 +3,53 @@ angular.module('mrtikitApp')
         var events = [
             {
                 'eventId': 1,
-                'name': 'Event 1'
+                'name': 'Event 1',
+                'image': 'http://placehold.it/1000x200/8565c4/ffffff?text=Event+1+Image'
             },
             {
                 'eventId': 2,
-                'name': 'Event 2'
+                'name': 'Event 2',
+                'image': 'http://placehold.it/1000x200/8565c4/ffffff?text=Event+2+Image'
             },
             {
                 'eventId': 3,
-                'name': 'Event 3'
+                'name': 'Event 3',
+                'image': 'http://placehold.it/1000x200/8565c4/ffffff?text=Event+3+Image'
             },
             {
                 'eventId': 4,
-                'name': 'Event 4'
+                'name': 'Event 4',
+                'image': 'http://placehold.it/1000x200/8565c4/ffffff?text=Event+4+Image'
             },
             {
                 'eventId': 5,
-                'name': 'Event 5'
+                'name': 'Event 5',
+                'image': 'http://placehold.it/1000x200/8565c4/ffffff?text=Event+5+Image'
             },
             {
                 'eventId': 6,
-                'name': 'Event 6'
+                'name': 'Event 6',
+                'image': 'http://placehold.it/1000x200/8565c4/ffffff?text=Event+6+Image'
             },
             {
                 'eventId': 7,
-                'name': 'Event 7'
+                'name': 'Event 7',
+                'image': 'http://placehold.it/1000x200/8565c4/ffffff?text=Event+7+Image'
             },
             {
                 'eventId': 8,
-                'name': 'Event 8'
+                'name': 'Event 8',
+                'image': 'http://placehold.it/1000x200/8565c4/ffffff?text=Event+8+Image'
             },
             {
                 'eventId': 9,
-                'name': 'Event 9'
+                'name': 'Event 9',
+                'image': 'http://placehold.it/1000x200/8565c4/ffffff?text=Event+9+Image'
             },
             {
                 'eventId': 10,
-                'name': 'Event 10'
+                'name': 'Event 10',
+                'image': 'http://placehold.it/1000x200/8565c4/ffffff?text=Event+10+Image'
             }];
         var getEvents = function () {
             return events;
