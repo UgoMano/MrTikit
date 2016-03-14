1af9f3cc9b80a8f0dfe5fd8f42c0dbe0429c287d
Stephen Calabro <steve.a.calabro@gmail.com>
2016-03-14 01:02:03 -0400

    #backOrExit

--- app/scripts/controllers/boxOffice/main.js
+++ app/scripts/controllers/boxOffice/main.js
@@ -7,8 +7,23 @@
  * # BoxOfficeMainCtrl
  * Controller of the mrtikitApp
  */
-angular.module('mrtikitApp').controller('BoxOfficeMainCtrl', function ($stateParams, $scope) {
-    console.log("Event" + $stateParams.id);
+angular.module('mrtikitApp').controller('BoxOfficeMainCtrl', function ($stateParams, $scope, $state, $location) {
     $scope.event = {};
     $scope.event.id = $stateParams.id;
+    
+    $scope.goBack = function() {
+        if($state.current.name == "boxOffice.home") {
+            $location.path("/dashboard");
+        } else {
+            $location.path("/boxOffice/"+$scope.event.id);
+        }
+    }
+    
+    $scope.backOrExit = function() {
+        if($state.current.name == "boxOffice.home") {
+            return "Exit";
+        } else {
+            return "Back";
+        }
+    }
 });
