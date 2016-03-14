a9fdaf957046e9cf559ea63a04f2ef91b6f2dd8b
Erin <nire46@gmail.com>
2016-03-14 01:10:36 -0400

    This is also a test

--- api/models/Events.js
+++ api/models/Events.js
@@ -3,7 +3,6 @@
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
- * test
  */
 
 module.exports = {
