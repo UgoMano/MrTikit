998471eacc0f9298d4fece12ad9aba270cf73ab9
Stephen Calabro <steve.a.calabro@gmail.com>
2016-03-13 21:53:25 -0400

    Added super basic ticket purchasing

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,6 +7,13 @@
  * # MainCtrl
  * Controller of the mrtikitApp
  */
-angular.module('mrtikitApp').controller('MainCtrl', function () {
-     //console.log('home')
+angular.module('mrtikitApp').controller('MainCtrl', function ($rootScope, $cookieStore) {
+    //console.log('home')
+    if (!$cookieStore.get("user") || !$cookieStore.get("loginKey")) {
+        //$location.path("/login");
+    } else {
+        $rootScope.user = $cookieStore.get("user");
+        $rootScope.user.loginKey = $cookieStore.get("loginKey");
+        $rootScope.user.loginType = $cookieStore.get("loginType");
+    }
 });
