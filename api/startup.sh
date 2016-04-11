165cdc82c588dfcd5396d59bfc0ebec1ce04ee8b
Steve Calabro <steve.a.calabro@gmail.com>
2016-04-11 17:10:23 -0400

    Added api-master instead of dev

--- api/startup.sh
+++ api/startup.sh
@@ -2,8 +2,8 @@
 cd /MrTikit
 export GIT_SSH=/root/.ssh/git_wrap.sh
 export EMAIL=joshbauer3@gmail.com
-git fetch origin api-dev --depth 1
-git reset --hard origin/api-dev
+git fetch origin api-master --depth 1
+git reset --hard origin/api-master
 #npm install -g bower
 #bower install --allow-root --force-latest
 npm install
