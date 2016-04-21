88d645c9f3607fe0010e6588d12741d9936418e9
Steve Calabro <steve.a.calabro@gmail.com>
2016-04-21 17:16:00 -0400

    My Ticket Page closes #141

--- MrTikit/MyTicketController.swift
+++ MrTikit/MyTicketController.swift
@@ -10,13 +10,83 @@ import UIKit
 import SwiftyJSON
 
 class MyTicketController: UIViewController {
+    @IBOutlet weak var imgQRCode: UIImageView!
+    @IBOutlet weak var eventTitle: UILabel!
+    @IBOutlet weak var startEndDate: UILabel!
+    @IBOutlet weak var ticketInfo: UILabel!
+    
     var myTicket:JSON!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+     
+        imgQRCode.image = generateCode(myTicket["scanId"].string!)
         
-        NSLog(myTicket.description)
-        // Do any additional setup after loading the view.
+        eventTitle.text = myTicket["event"]["title"].string!
+        
+        
+        //Fill the date out
+        var startDate:String = ""
+        var endDate:String = ""
+        
+        if(myTicket["event"]["startDateTime"].string! != "") {
+            startDate = toDateTime(myTicket["event"]["startDateTime"].string!)
+        }
+        
+        if(myTicket["event"]["endDateTime"].string! != "") {
+            endDate = toDateTime(myTicket["event"]["endDateTime"].string!)
+        }
+        
+        if(endDate == "") {
+            startEndDate.text = startDate
+        } else {
+            startEndDate.text = startDate + " - " + endDate
+        }
+        
+        ticketInfo.text = "$" + myTicket["ticketType"]["price"].int!.description + " " + myTicket["ticketType"]["name"].string!
+    }
+    
+    func generateCode(input: String) -> UIImage {
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        
+        let data = input.dataUsingEncoding(NSUTF8StringEncoding)
+        filter!.setValue("H", forKey:"inputCorrectionLevel")
+        filter!.setValue(data, forKey:"inputMessage")
+        
+        let outputImage = filter!.outputImage
+        let context = CIContext(options:nil)
+        let cgImage = context.createCGImage(outputImage!, fromRect:outputImage!.extent)
+        
+        let image = UIImage(CGImage:cgImage, scale:1.0, orientation:UIImageOrientation.Up)
+        let resized = resizeImage(image, withQuality: CGInterpolationQuality.None, rate:5.0)
+        return resized
+    }
+    
+    func resizeImage(image: UIImage, withQuality quality: CGInterpolationQuality, rate: CGFloat) -> UIImage {
+        let width = image.size.width * rate
+        let height = image.size.height * rate
+        
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(width, height), true, 0)
+        let context = UIGraphicsGetCurrentContext()
+        CGContextSetInterpolationQuality(context, quality)
+        image.drawInRect(CGRectMake(0, 0, width, height))
+        
+        let resized = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();        
+        return resized;
+    }
+    
+    func toDateTime(input: String) -> String
+    {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        let localDate = formatter.dateFromString(input)
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy h:mm a"
+        let DateInFormat = dateFormatter.stringFromDate(localDate!)
+        
+        return DateInFormat
     }
     
     override func didReceiveMemoryWarning() {
