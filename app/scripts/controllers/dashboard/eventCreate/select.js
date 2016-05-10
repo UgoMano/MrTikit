2e8b3e493febc188fa6369c6f938952cdef7b920
Josh Bauer <jbauer@Joshs-MacBook-Pro.local>
2016-05-10 18:20:00 -0400

    show facebook events

--- app/scripts/controllers/dashboard/eventCreate/select.js
+++ app/scripts/controllers/dashboard/eventCreate/select.js
@@ -7,7 +7,7 @@
  * # EventCreateSelectCtrl
  * Controller of the mrtikitApp
  */
-angular.module('mrtikitApp').controller('EventCreateSelectCtrl', function ($scope, $User, $Event, $mdToast, $filter) {
+angular.module('mrtikitApp').controller('EventCreateSelectCtrl', function ($scope, $User, $Event, $mdToast, $filter,Facebook) {
     $scope.setStep(0);
     $scope.events = [];
     $scope.unpublished = [];
@@ -20,9 +20,32 @@ angular.module('mrtikitApp').controller('EventCreateSelectCtrl', function ($scop
         //$mdToast.showSimple('success');
         console.log(events);
         $scope.events = events;
-        $scope.unpublished = $filter('filter')(events, { published: false});
+        $scope.unpublished = $filter('filter')(events, {
+            published: false
+        });
     }, function (error) {
         $mdToast.showSimple('error');
         console.log(error);
+    });
+    Facebook.getLoginStatus(function (response) {
+        console.log(response.authResponse.accessToken);
+
+        if (response.status === 'connected') {
+            $User.getFacebookEvents($scope.user.facebookId, response.authResponse.accessToken).then(function (data) {
+                    console.log(data);
+                    $scope.fbevents=data.data.data;
+                    console.log(JSON.stringify($scope.fbevents,null,' '));
+                },
+                function (error) {
+                    if (error.error) {
+                        $mdToast.showSimple(error.error);
+                    } else if (error.status == 401) {
+                        //$mdToast.showSimple(error.data.message);
+                        console.log(error);
+                    } else {
+                        console.log(error);
+                    }
+                });
+        }
     });
 });
