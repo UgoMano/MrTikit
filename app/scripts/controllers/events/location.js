8950605fe676caac96cd2905e40f7f97d8079684
Steve Calabro <steve.a.calabro@gmail.com>
2016-05-29 21:34:25 -0400

    Added location front end

--- app/scripts/controllers/events/location.js
+++ app/scripts/controllers/events/location.js
@@ -8,15 +8,5 @@
  * Controller of the mrtikitApp
  */
 angular.module('mrtikitApp').controller('EventLocationCtrl', function ($scope, $stateParams) {
-    $scope.map = {
-        center: { 
-            latitude: 39.956091,
-            longitude: -75.188024
-        },
-        zoom: 17,
-        marker: {
-            latitude: 39.956091,
-            longitude: -75.188024
-        }
-    };
+   
 });
